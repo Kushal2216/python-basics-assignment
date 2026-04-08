3ab6340ebc8785638ffeba2180c89f320d8a4e82
Kushal2216 <pkushalbabu.adams@gmail.com>
2026-04-08 23:26:40 +0530

    Add files via upload

---
+++ python_basics_kushal.py
@@ -0,0 +1,12 @@
+name=input("Enter Name:")
+age=int(input("Age:"))
+height=float(input("Height:"))
+is_student = True
+print(f"{name},{age},{height},{is_student}")
+
+print(f"Name: {name} | Age: {age} | Height: {height} |Student: {is_student}")
+
+print("in_Months:",age*12)
+print("in_Days:",age*365)
+print("Div:",age/7)
+print("Power:",age**2)
